23981138014e3dc9f554854bb192b46c72e3b233
PC z370 <iapad777@gmail.com>
2018-05-30 10:31:19 +0300

    template

---
+++ Source/MMVR/Public/MMGameInstance.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "MMGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MMVR_API UMMGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
